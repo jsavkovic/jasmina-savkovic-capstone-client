f1fa5ef7a5793b6ce15ed9a063182783f3e32756
Jasmina Savkovic <jsavkovi@gmail.com>
2024-07-07 02:26:14 -0400

    Set up page routes. :qa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.scss'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <p>Homepage</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/lend' element={<LendList />} />
+        <Route path='/lend/:itemId' element={<ItemDetails />} />
+        <Route path='/gift' element={<GiftList />} />
+        <Route path='/gift/:itemId' element={<ItemDetails />} />
+        <Route path='/friends' element={<FriendList />} />
+        <Route path='/friend-requests' element={<FriendRequestList />} />
+        <Route path='/borrow-request' element={<BorrowRequestList />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
