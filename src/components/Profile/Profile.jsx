ee44c9ba70a1f20c63940e8b5f656e5656ef1d35
Jasmina Savkovic <jsavkovi@gmail.com>
2024-07-10 18:46:57 -0400

    Dynamically render user name and email on profile component.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,9 +4,37 @@ import archiveIcon from '../../assets/icons/archive.svg';
 import uploadIcon from '../../assets/icons/upload.svg';
 import downloadIcon from '../../assets/icons/download.svg';
 import listIcon from '../../assets/icons/list.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const Profile = () => {
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const userId = 1 // replace with logged in user prop when log in is set up
+                console.log(`Fetching user data from URL: ${API_URL}/user/${userId}`)
+                const response = await axios.get(`${API_URL}/user/${userId}`);
+                const user = response.data;
+
+                if (user) {
+                    setUserName(user.first_name);
+                    setUserEmail(user.email);
+                } else {
+                    setUserName('User not found');
+                    setUserEmail('');
+                }
+            } catch (err) {
+                console.error('Error fetching user data:', err)
+            }
+        };
+        fetchUserData();
+    }, [API_URL]);
+
     return (
         <main className='profile'>
             <div className='profile__top'>
@@ -16,8 +44,8 @@ const Profile = () => {
                 </div>
                 <div className='profile__avatar'></div>
                 {/* dynamically render user first name */}
-                <h2 className='profile__subtitle'>Welcome back User!</h2>
-                <p className='profile__email'>Pull in user email address</p>
+                <h2 className='profile__subtitle'>Welcome back {userName}!</h2>
+                <p className='profile__email'>{userEmail}</p>
             </div>
             <div className='profile__nav'>
                 <img className='profile__icons' src={listIcon} alt='list items' />
