e99312e27c27627ce4217f045eef3fb0b94a54d3
Jasmina Savkovic <jsavkovi@gmail.com>
2024-07-11 15:49:11 -0400

    Create friend request component and styling. Render in profile.

--- src/components/BorrowRequests/BorrowRequests.jsx
+++ src/components/BorrowRequests/BorrowRequests.jsx
@@ -25,7 +25,7 @@ const BorrowRequests = ({ userId }) => {
             <div className='borrow-req__table'>
                 <div className='borrow-req__header'>
                     <div>Image</div>
-                    <div>Name</div>
+                    <div>Item</div>
                     <div>Borrower</div>
                     <div>Requested On</div>
                     <div>Actions</div>
@@ -33,16 +33,30 @@ const BorrowRequests = ({ userId }) => {
                 {borrowRequests.length > 0 ? (
                     borrowRequests.map(request => (
                         <div className='borrow-req__row' key={request.id}>
-                            <div><img src={`${API_URL}/uploads/${request.item_image}`} alt={request.item_name} className='borrow-req__image' /> </div>
-                            <div>{request.item_name}</div>
-                            <div>{request.borrower_first_name} {request.borrower_last_name}</div>
-                            <div>{new Date(request.created_at).toLocaleDateString()}</div>
-                            <div><button>Action</button></div>
+                            <div className='borrow-req__details'>
+                                {request.item_image ? (
+                                    <img src={`${API_URL}/uploads/${request.item_image}`} alt={request.item_name} className='borrow-req__image' />
+                                ) : (
+                                    'No Image'
+                                )}
+                            </div>
+                            <div className='borrow-req__details'>
+                                {request.item_name}
+                            </div>
+                            <div className='borrow-req__details'>
+                                {request.borrower_first_name} {request.borrower_last_name}
+                            </div>
+                            <div className='borrow-req__details'>
+                                {request.created_at ? new Date(request.created_at).toLocaleDateString() : 'Date not available'}
+                            </div>
+                            <div className='borrow-req__details'>
+                                <button className='borrow-req__action'>Accept</button>
+                                <button className='borrow-req__action borrow-req__action--ignore'>Decline</button>
+                            </div>
                         </div>
                     ))
                 ) : (
                     <div className='borrow-req__row'>No borrow requests found</div>
-
                 )}
             </div>
         </section>
